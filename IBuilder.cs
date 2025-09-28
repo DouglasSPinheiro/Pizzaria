2e1436e9a9aab5664db5fd67a9a4429a68dab46e
DouglasSPinheiro <douglas.ss.pinheiro@gmail.com>
2025-09-28 13:39:38 -0300

    Update IBuilder.cs

--- IBuilder.cs
+++ IBuilder.cs
@@ -9,7 +9,9 @@ namespace Pizzaria
     public interface IBuilder
     {
         void Reset();
-        void SetTaste(string taste, string tast2, string tast3);
+        //substitua metodo setTaste pelo addTaste e crie uma lista de sabores dentro de pizza
+        //void SetTaste(string taste, string tast2, string tast3); 
+        void addTaste(string newtaste); 
         void SetEdge(string edge);
         void SetSize(string size);
     }
